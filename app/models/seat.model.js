3a87b6465b358a176af21731de116939b5fc0d0a
orthjs <zzxcasd64052@gmail.com>
2020-12-26 23:54:42 +0800

    新增增加座位功能

--- app/models/seat.model.js
+++ app/models/seat.model.js
@@ -4,6 +4,19 @@ const Seat = function (seat) {
     this.showing_id = showing.showing_id;
 }
 
+Seat.addSeat = (seatData, result) =>{
+    //新增哪場showing中被選中的那些座位 ["A1", "A2", "A3"] 
+    console.log("3. seatData:" + seatData);
+    sql.query(`INSERT INTO SEAT (seat_row_column, showing_id) VALUES ${seatData};`, (err, res) => {
+        if (err) {
+            result(null, err);
+            return;
+        }
+        console.log("新增seat成功");
+        result(null, res);
+    });
+}
+
 Seat.getSeatByShowingId = (showing_id, result) => {
     console.log("==================================================" + showing_id);
     sql.query(`Select seat.id, seat.seat_row_column
--- app/routes/seat.routes.js
+++ app/routes/seat.routes.js
@@ -4,4 +4,7 @@ const seats = require("../controllers/seat.controller.js");
 
 router.post("/seat/fetchSeatByShowingId", seats.findSeatByShowingId);
 
+router.post("/seat/addSeat", seats.addSeat);
+//新增哪場showing中被選中的那些座位，傳入資料格式為{"selectedSeat": ["A1","A2","A3"],"showingId": 48}
+
 module.exports = router;
