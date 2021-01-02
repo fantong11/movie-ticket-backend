7a86ef9cc46ba3c2a45433e755ebb8b10288837e
orthjs <zzxcasd64052@gmail.com>
2021-01-02 21:53:22 +0800

    新增product, 新addSeat, order_product功能

--- app/controllers/seat.controller.js
+++ app/controllers/seat.controller.js
@@ -2,11 +2,12 @@ const Seat = require("../models/seat.model.js");
 
 exports.addSeat = (req, res) => {
     //新增哪場showing中被選中的那些座位 ["A1", "A2", "A3"] 
+    // 目前不會呼叫這個，而是透過order那裡直接加
     let seatNameList = req.body.seat;
     let showingId = req.body.showingId;
     let seatData = "";
     //["A1","A2","A3"] ->  "("A1", 10), ("A2", 10), ("A3", 10)"
-    console.log("1. seatNameList: " + req.body.selectedSeat);
+    console.log("1. seatNameList: " + req.body.seat);
     console.log("2. showingId: " + req.body.showingId);
     for (let i = 0; i < seatNameList.length; i++) {
         //整合資料
@@ -19,6 +20,7 @@ exports.addSeat = (req, res) => {
         if (err) {
             return res.status(500).send({message: err.message});
         }
+        console.log("InsertedId: " + (data.insertId) + "~" + (data.insertId + data.affectedRows -1))
         res.send(data)
     })
 }
--- app/models/seat.model.js
+++ app/models/seat.model.js
@@ -1,12 +1,14 @@
 const sql = require("./db.js");
 
 const Seat = function (seat) {
-    this.showing_id = showing.showing_id;
+    this.seat_row_column = seat.seat_row_column;
+    this.showing_id = seat.showing_id;
+    this.order_id = seat.order_id;
 }
 
 Seat.addSeat = (seatData, result) =>{
-    //新增哪場showing中被選中的那些座位 ["A1", "A2", "A3"] 
-    console.log("3. seatData:" + seatData);
+    // 新增哪場showing中被選中的那些座位 ["A1", "A2", "A3"] 
+    // 目前不會呼叫這個，而是透過order那裡直接加
     sql.query(`INSERT INTO SEAT (seat_row_column, showing_id) VALUES ${seatData};`, (err, res) => {
         if (err) {
             result(null, err);
