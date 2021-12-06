2d991062ed1da638f6ab9c5306c0fb42a1887fdb
Fantong <fankaihsiang11@gmail.com>
2021-12-06 15:27:02 +0800

    fix database

--- app/models/db.js
+++ app/models/db.js
@@ -4,16 +4,14 @@ const dbConfig = require("../config/db.config.js");
 // Create a connection to the database
 const connection = mysql.createConnection({
   host: dbConfig.HOST,
-  user: dbConfig.USERNAME,
+  user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DATABASE,
+  database: dbConfig.DB,
   port: dbConfig.PORT
 });
 
 // open the MySQL connection
 connection.connect(error => {
-  console.log(dbConfig.HOST + " 70986543678908765436");
-
   if (error) throw error;
   console.log("Successfully connected to the database.");
 });
